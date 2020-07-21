aea167678770704825aa6b7f1efecbddcd6f3408
Luis Hernandez <>
2020-07-21 16:45:29 -0500

    init commit

---
+++ InfColorPickerSample/ViewController.cs
@@ -0,0 +1,36 @@
+﻿using Foundation;
+using InfColorPicker;
+using System;
+using UIKit;
+
+namespace InfColorPickerSample
+{
+    public partial class ViewController : UIViewController
+    { 
+        ColorSelectedDelegate selector;
+
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+            ChangeColorButton.TouchUpInside += HandleTouchUpInsideWithStrongDelegate;
+            selector = new ColorSelectedDelegate(this);
+        }
+        private void HandleTouchUpInsideWithStrongDelegate(object sender, EventArgs e)
+        {
+            InfColorPickerController picker = InfColorPickerController.ColorPickerViewController;
+            picker.Delegate = selector;
+            picker.PresentModallyOverViewController(this);
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
